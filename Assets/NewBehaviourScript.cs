c67e9ae1cdda29d87ae000c1833d5ec1c926f095
Windtalker4life <mitchellredhouse@hotmail.com>
2019-07-25 01:48:57 -0500

    Spaceship and Terrain Added

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NewBehaviourScript : MonoBehaviour
+{
+    public static AudioSource audioSource;
+    enum Button { PlayGame, Options, Exit, LevelSelect }
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+    void Start()
+    {
+        Invoke("Game",2f);
+    }
+
+    private void Game()
+    {
+        SceneManager.LoadScene(1);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
