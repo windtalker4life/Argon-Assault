79ffd412fe99efb5b467b18760200289c314f325
Windtalker4life <mitchellredhouse@hotmail.com>
2019-08-29 04:12:30 -0500

    Git Gui

---
+++ Assets/DestroyWhenHit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWhenHit : MonoBehaviour
+{
+    [SerializeField] GameObject Deathfx;
+    [SerializeField] Transform parent;
+    [SerializeField] int scoreperhit = 1;
+    // Start is called before the first frame update
+    scoreBoard ScoreBoard;
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+        ScoreBoard = FindObjectOfType<scoreBoard>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void AddNonTriggerBoxCollider()
+    {
+        BoxCollider enemyCollider = gameObject.AddComponent<BoxCollider>();
+        enemyCollider.isTrigger = false;
+        enemyCollider.center = new Vector3(0f, 0f, 0f);
+        enemyCollider.size = new Vector3(.2f, .5f, .3f);
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        ScoreBoard.ScorePerHit(scoreperhit);
+        GameObject fx = Instantiate(Deathfx, transform.localPosition,Quaternion.identity);
+        fx.transform.parent = parent;
+        print("particle hit " + gameObject.name);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/scoreBoard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class scoreBoard : MonoBehaviour
+{
+    [SerializeField] int score;
+    Text scoretext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoretext = GetComponent<Text>();
+        scoretext.text = score.ToString();
+    }
+    public void ScorePerHit(int scoreincrease)
+    {
+        score = score + scoreincrease;
+        scoretext.text = score.ToString();
+        // takes score then adds scoreperhit
+        // displays as new scoretext as a string
+    }
+}
